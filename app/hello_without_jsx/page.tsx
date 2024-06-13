718e561df5f8c7cfbb21e58c00c17d65c2736147
kenichi04 <slow_life_1407@yahoo.co.jp>
2024-06-13 20:53:21 +0000

    4-1-3frontend state

---
+++ app/hello_without_jsx/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Page() {
+    return React.createElement('h1', null, `Hello, Next.js! without JSX`);
+}
--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,14 @@
 const nextConfig = {};
 
 export default nextConfig;
+
+const rewrites = async () => {
+    return [
+        {
+            source: 'api/:path*',
+            destination: 'http://host.docker.internal:8000/api/:path*',
+        },
+    ];
+};
+
+export { rewrites };
