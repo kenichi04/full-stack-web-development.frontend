3ba401a28ab364f341fff6a1e9d1dab370c323e6
kenichi04 <slow_life_1407@yahoo.co.jp>
2024-06-15 21:32:51 +0000

    api fetch test from backend

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+console.log("Loading next.config.mjs");
 
-const rewrites = async () => {
-    return [
-        {
-            source: 'api/:path*',
-            destination: 'http://host.docker.internal:8000/api/:path*',
-            // destination: '192.168.11.3:8000/api/:path*',
-        },
-    ];
+const nextConfig = {
+    // async rewrites() {
+    //     return [
+    //         {
+    //             source: '/api/',
+    //             destination: 'http://host.docker.internal:8000/api',
+    //         }
+    //     ]
+    // }
 };
 
-export { rewrites };
+export default nextConfig;
